432d4309808bf703edfa616488f4823ec9d8881b
Somrat Hossen <33490220+Somrat-Hossen@users.noreply.github.com>
2018-03-29 18:00:44 +0600

    Add files via upload

---
+++ Number conversion/decimal to binary.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+         {
+             int decimal,array[100],i=0,j,cnt=0,arr[100];
+             printf("Enter decimal\n");
+             scanf("%d",&decimal);
+
+           cout<<binary(decimal);
+
+           printf("\n%d",cnt);
+
+
+
+
+          return 0;
+         }
